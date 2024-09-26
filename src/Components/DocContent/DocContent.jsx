f5adc0fc7cef0e4a70dcfdc31e29bb6ca1355052
prathamsaxen <er.prathamsaxena@gmail.com>
2024-09-26 15:17:37 +0530

    Blue Theme

---
+++ src/Components/DocContent/DocContent.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./DocContent.css";
+const DocContent = () => {
+  return (
+    <div className="doc-content">
+      <h2>Page Content</h2>
+      <ul>
+        <li>Overview</li>
+        <li>What is Truxt?</li>
+        <li>How Does Truxt.ai Works?</li>
+        <li>How do i get with started?</li>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
+
+export default DocContent;
