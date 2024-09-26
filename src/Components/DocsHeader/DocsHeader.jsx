eb1e2311cb783efffa62768273b0d056f25bf6cc
prathamsaxen <er.prathamsaxena@gmail.com>
2024-09-26 14:22:14 +0530

    Fixed Components

--- src/Components/DocsHeader/DocsHeader.jsx
+++ src/Components/DocsHeader/DocsHeader.jsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React from "react";
 import "./DocsHeader.css";
 import logo from "../../assets/header/truxt_logo_login.png";
+import { NavLink } from "react-router-dom";
 
 function DocsHeader() {
+  const NavigationItems = [
+    {
+      path: "/",
+      title: "Home",
+    },
+    {
+      path: "http://dev.truxt.xyz/",
+      title: "Sandbox",
+    },
+  ];
+
   return (
-    <div className='Docs-Header'>
+    <div className="Docs-Header">
       <div className="left-docs-header">
         <img src={logo} alt="Error in loading..." />
       </div>
+      <div className="right-docs-header">
+        <input type="search" name="Search" id="" placeholder="Search in docs" />
+        <nav>
+          <ul>
+            {NavigationItems.map((item, _) => {
+              return (
+                <NavLink to={item.path} key={item.title}>
+                  {item.title}
+                </NavLink>
+              );
+            })}
+          </ul>
+        </nav>
+      </div>
     </div>
-  )
+  );
 }
 
-export default DocsHeader
+export default DocsHeader;
